e90c9cf8bb5ee89bba23642604ecab2c3d730178
Pablo <gpabloandres@gmail.com>
2023-07-31 11:36:50 -0300

    Actualiza repo

---
+++ Python/miPrimerProgramaPython.py
@@ -0,0 +1,19 @@
+print("Hola mundo!")
+
+numero1 = int(input("Introduzca el numero1: "))
+numero2 = int(input("Introduzca el numero2: "))
+
+dividir = numero1 / numero2
+print("La división de ambos números es:")
+print(dividir)
+
+print("###############################")
+print("EL programa genera mi nombre")
+print("###############################")
+
+primerNombre = input("¿Cuál es tu primer nombre?: ")
+segundoNombre = input("¿Cuál es tu segundo nombre?: ")
+apellido = input("¿Cuál es tu apellido?: ")
+
+print(f"Tu nombre es {primerNombre} {segundoNombre} {apellido}")
+
